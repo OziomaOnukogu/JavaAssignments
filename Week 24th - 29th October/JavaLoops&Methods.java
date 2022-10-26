d5530bf538fc2e62c16ab79170143afc02359d86
OziomaOnukogu <116321022+OziomaOnukogu@users.noreply.github.com>
2022-10-26 16:20:37 +0000

    Adding Java Assignments for October 24th - 29th

---
+++ Week 24th - 29th October/JavaLoops&Methods.java
@@ -0,0 +1,17 @@
+public class dog {
+
+    String name = ""?
+'
+}
+/**
+ * Hello world!
+ * 
+//  *
+public static void main (string [] args)
+{
+
+
+    Dog  bruce = new Dog ();
+    bruce.run();
+    Dog.bark();
+}
---
+++ Week 24th - 29th October/TryingMethods.java
@@ -0,0 +1,13 @@
+public class TryingMethods {
+	//Main method
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/*	If you want to execute a method 
+	 * 	Just Call it inside the main method .
+	 * Like This 
+	 */
+		
+	}
+		
+
+}
